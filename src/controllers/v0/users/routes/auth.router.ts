37ca826b7d7f5b590b4dbb9ad77a43b62baa0476
Sergio Pichardo <sp88work@gmail.com>
2020-04-07 15:12:36 -0500

    implement generate hashed password and compare password auth functions

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -11,13 +11,16 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return ""
+  //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(plainTextPassword, salt)
+    return hash
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
+    return await bcrypt.compare(plainTextPassword, hash)
 }
 
 function generateJWT(user: User): string {
@@ -114,6 +117,7 @@ router.post('/', async (req: Request, res: Response) => {
     });
 
     let savedUser;
+
     try {
         savedUser = await newUser.save();
     } catch (e) {
